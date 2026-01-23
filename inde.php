c6e09f411ccc1724117b679d0ae0c98f03d48a80
ashishkuma8847 <ashishkuma8847@gmail.com>
2026-01-23 11:44:47 +0530

    first commit

---
+++ inde.php
@@ -0,0 +1,33 @@
+<?php
+// // connection using mysqli
+// $host ="localhost";
+// $username="root";
+// $password=null;
+// $database="first";
+// $connect = new mysqli( $host,$username,$password,$database);
+// if($connect->connect_error){
+//     return ( "data base not connected".$connect->connect_error);
+// }else{
+//     echo "connected with database";
+// };
+// $res = $connect->query("show tables")->fetch_all();
+// print_r($res)
+
+// // connection using PDO
+
+$host="localhost";
+$username="root";
+$password=null;
+$database="first";
+
+try {
+    $connect = new PDO(
+        "mysql:host=$host;dbname=$database",$username,$password
+    );
+    $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "database connected";
+} catch (PDOException$err) {
+echo "connection failed" .$err ->getMessage();
+}
+$res = $connect=>query("show tables")
+?>
